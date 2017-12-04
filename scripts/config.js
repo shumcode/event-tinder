1989d093f36182ae606bdb5088ed5ff45f95ea7f
Michelle Williams <mattie.williams31@gmail.com>
2017-12-04 10:44:53 -0500

    Added header and set up config and module

--- scripts/config.js
+++ scripts/config.js
@@ -1,1 +1,13 @@
-//Andrew has the congif file///
+(function(){
+
+  angular
+    .module("app")
+    .config(function($routeProvider){
+      $routeProvider
+      .when("/hello", {
+        template:`
+          <h1>HI</h1>
+        `
+      })
+    })
+})();
