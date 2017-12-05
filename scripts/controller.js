e6744137941a9db8ffd1ad02fbd4a6bbe074d331
shumcode <cshumylo@gmail.com>
2017-12-05 11:39:33 -0500

    added new checkboxes

--- scripts/controller.js
+++ scripts/controller.js
@@ -2,6 +2,7 @@
   function QuestionController(EventService){
     var vm = this;
     vm.rmessage;
+    // vm.events;
     vm.routeFunc= function(choice){
       EventService.routeFunc(choice);
     }
@@ -9,9 +10,13 @@
     vm.object = EventService.returnObj();
 
     EventService.makeRequest().then(function(response) {
-      // console.log(response);
-      vm.events = response.data._embedded.events
+      console.log(response);
+      // vm.events = response.data._embedded.events
     })
+
+    vm.getCity = function(location){
+      EventService.cityFunc(location);
+    } 
   }/*End of Controller*/
 
   angular
